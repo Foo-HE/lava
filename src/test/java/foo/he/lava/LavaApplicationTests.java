b6c844620ff1b4f93038e1a85486da5e285f8816
Hector Amoah <amoahhector@live.com>
2021-08-16 19:18:02 +0000

    Spring Boot Initialised

---
+++ src/test/java/foo/he/lava/LavaApplicationTests.java
@@ -0,0 +1,13 @@
+package foo.he.lava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
